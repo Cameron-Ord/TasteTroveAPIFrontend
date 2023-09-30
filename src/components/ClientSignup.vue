60f1cc5954b62b26038ba1f99a0640843909b932
Cameron-Ord <cameron.ian.ronald.ord@outlook.com>
2023-09-29 23:49:20 -0400

    signup component

---
+++ src/components/ClientSignup.vue
@@ -0,0 +1,55 @@
+
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        components:{
+
+        },
+
+        data() {
+            return {
+                
+            }
+        },
+
+        methods:{
+
+        },
+        computed:{
+
+        },
+        created(){
+
+        },
+        mounted(){
+
+        },
+        beforeMount(){
+
+        },
+        beforeUpdate(){
+
+        },
+        updated(){
+
+        },
+        beforeUnmount(){
+
+        },
+        unmounted(){
+
+        }
+
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
+
